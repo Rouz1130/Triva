74a19922c7a2401aec4dc3d31e1b5786c0c48d1e
rouzbeh majlessi <rmajlessi@gmail.com>
2018-07-04 02:51:45 -0700

    Add category service to GET json from Url as Observable

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import '../rxjs-extensions';
+
 
 @Injectable()
 export class CategoryService {
+  private _serviceUrl = 'http://localhost:3000/categories';
 
-  constructor() { }
+  constructor(private http: Http) {
+  }
 
+
+  getCategories(): Observable<Category[]> {
+    let url = this._serviceUrl;
+    return this.http.get(url)
+      .map(res => res.json());
+  }
 }
